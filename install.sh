260a59c78da3bd236218fb9d5149b65b96fd59ef
Quan To <btquanto@gmail.com>
2017-11-03 13:50:29 +0700

    Generate config.json when not available

--- install.sh
+++ install.sh
@@ -25,10 +25,24 @@ cd "$bin_dir";
 # Check if config.json exists
 if [ ! -f "config.json" ]
 then
-   echo "Config file not found. Create your config.json file based on config.json.template";
-   exit 0;
+    echo "Config file not found. Creating one...";
+    echo
+    echo "===== Configuring git ===="
+    echo -n 'User: '
+    read git_user
+    echo -n 'Email: '
+    read git_email
+    echo "\
+{
+	\"_gitconfig\" : {
+		\"name\" : \"$git_user\",
+        \"email\" : \"$git_email\"
+	}
+}\
+" > config.json
+    echo "Config file has been generated"
 else
-   echo "Config file found. Proceeding to install the dotfiles";
+    echo "Config file found. Proceeding to install the dotfiles";
 fi;
 
 # Check if virtualenv exists. If not, install it. Exit if installation failed.
@@ -37,26 +51,33 @@ then
 	echo "Virtualenv is found";
 else
 	echo "Virtualenv is not found. Proceed with installing virtual environment";
-    if [ $(whoami) == "root" ]
-    then
-        apt-get install python-virtualenv -y;
-    else
-        if [ "$EUID" -ne 0 ]
+    if hash apt-get 2> /dev/null;
+    then 
+        if [ $(whoami) == "root" ]
         then
-            if hash sudo 2> /dev/null
+            apt-get install python-virtualenv -y;
+        else
+            if [ "$EUID" -ne 0 ]
             then
-                sudo apt-get install python-virtualenv -y;
+                if hash sudo 2> /dev/null
+                then
+                    sudo apt-get install python-virtualenv -y;
+                fi;
             fi;
         fi;
-    fi;
 
-	if hash virtualenv 2> /dev/null;
-	then
-		echo "Virtualenv is installed"
-	else
-		echo "Virtualenv is not installed. Abort the installation of the dot files";
-		exit 0;
-	fi;
+        if hash virtualenv 2> /dev/null;
+        then
+            echo "Virtualenv is installed";
+        else
+            echo "Virtualenv is not installed. Abort the installation of the dot files";
+            exit 0;
+        fi;
+    else
+        echo "This script currently only support linux distros with apt-get";
+        echo "If you wish to continue, please proceed to install virtualenv manually";
+        exit 0;
+    fi;
 fi;
 
 # Check if any virtual environment has been initialized
